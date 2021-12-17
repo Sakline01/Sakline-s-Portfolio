a76184833714b820018e06e46ab02a58d38e4f25
Sakline Sk <Sakline_pw1_056@masai.school>
2021-12-17 18:27:42 +0530

    Updates

---
+++ precache-manifest.9237c23aaec15110d929b405c3389e10.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f29bd532d5815716fd92d92731caa25",
+    "url": "/Sakline-s-Portfolio/index.html"
+  },
+  {
+    "revision": "5cbb2ef8a980347df528",
+    "url": "/Sakline-s-Portfolio/static/css/main.724ccd54.chunk.css"
+  },
+  {
+    "revision": "3cf36565de235c629129",
+    "url": "/Sakline-s-Portfolio/static/js/2.1d0b4109.chunk.js"
+  },
+  {
+    "revision": "5cbb2ef8a980347df528",
+    "url": "/Sakline-s-Portfolio/static/js/main.b6d85d70.chunk.js"
+  },
+  {
+    "revision": "a0b0cb80c285e9e578dd",
+    "url": "/Sakline-s-Portfolio/static/js/runtime-main.273c454a.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Sakline-s-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Sakline-s-Portfolio/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Sakline-s-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/Sakline-s-Portfolio/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/Sakline-s-Portfolio/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "0f956f73d4042c80416e2c4b011ae2c7",
+    "url": "/Sakline-s-Portfolio/static/media/bhs.0f956f73.png"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/Sakline-s-Portfolio/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "0e9dcfdce936c04767a6cefde702bab3",
+    "url": "/Sakline-s-Portfolio/static/media/circle.0e9dcfdc.png"
+  },
+  {
+    "revision": "f2b8403232dd7b7e23080c84e3ad337f",
+    "url": "/Sakline-s-Portfolio/static/media/circleimage.f2b84032.png"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/Sakline-s-Portfolio/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/Sakline-s-Portfolio/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/Sakline-s-Portfolio/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/Sakline-s-Portfolio/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/Sakline-s-Portfolio/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "2814ecb1258d1326f2c6a04496eb4f5d",
+    "url": "/Sakline-s-Portfolio/static/media/download.2814ecb1.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/Sakline-s-Portfolio/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/Sakline-s-Portfolio/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/Sakline-s-Portfolio/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/Sakline-s-Portfolio/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/Sakline-s-Portfolio/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/Sakline-s-Portfolio/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/Sakline-s-Portfolio/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/Sakline-s-Portfolio/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/Sakline-s-Portfolio/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/Sakline-s-Portfolio/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/Sakline-s-Portfolio/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/Sakline-s-Portfolio/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/Sakline-s-Portfolio/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/Sakline-s-Portfolio/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/Sakline-s-Portfolio/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/Sakline-s-Portfolio/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/Sakline-s-Portfolio/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/Sakline-s-Portfolio/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/Sakline-s-Portfolio/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/Sakline-s-Portfolio/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/Sakline-s-Portfolio/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/Sakline-s-Portfolio/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/Sakline-s-Portfolio/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/Sakline-s-Portfolio/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "1c9a418f8a37477c90d3d144c95874f4",
+    "url": "/Sakline-s-Portfolio/static/media/masai.1c9a418f.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/Sakline-s-Portfolio/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/Sakline-s-Portfolio/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/Sakline-s-Portfolio/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/Sakline-s-Portfolio/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "d5379ce82f50a4fa3533298fa0b35ca0",
+    "url": "/Sakline-s-Portfolio/static/media/sakuimage.d5379ce8.jpg"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/Sakline-s-Portfolio/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/Sakline-s-Portfolio/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/Sakline-s-Portfolio/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
